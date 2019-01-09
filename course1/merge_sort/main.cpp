9fbd8088b5977ac145fe74fad5f6fffa5d29e79b
Clayton Wong <clayton.wong@ookla.com>
2019-01-09 09:20:40 -0800

    merge_sort: use first class concept Collection and minor cleanup to change the iterator names for better readability

--- course1/merge_sort/main.cpp
+++ course1/merge_sort/main.cpp
@@ -9,31 +9,31 @@ using namespace std;
 template< typename T >
 class Solution
 {
-    using Vector = vector< T >;
-    using Iter = typename Vector::iterator;
+public:
 
-    Vector go( Vector&& A )
+    using Collection = vector< T >;
+
+    Collection mergeSort( const Collection& A )
     {
-        auto N{ A.size() };
-        if( N < 2 ) return A;
-        auto pivot{ A.cbegin() + N / 2 };
+        return go({ A.cbegin(), A.cend() });
+    }
+
+private:
+
+    Collection go( Collection&& A )
+    {
+        if( A.size() < 2 ) return A;
+        auto pivot = A.cbegin() + A.size() / 2;
         return merge( go({ A.cbegin(), pivot }), go({ pivot, A.cend() }) );
     }
 
-    Vector merge( const Vector& lhs, const Vector& rhs, Vector res={} )
+    Collection merge( const Collection& lhs, const Collection& rhs, Collection res={} )
     {
-        auto i{ lhs.cbegin() }, j{ rhs.cbegin() };
-        while( i != lhs.cend() && j != rhs.cend() ) res.push_back( ( *i < *j )? *i++ : *j++ );
-        if( i != lhs.cend() ) res.insert( res.end(), i, lhs.cend() );
-        if( j != rhs.cend() ) res.insert( res.end(), j, rhs.cend() );
+        auto L{ lhs.cbegin() }, R{ rhs.cbegin() };
+        while( L != lhs.cend() && R != rhs.cend() ) res.push_back( ( *L < *R )? *L++ : *R++ );
+        if( L != lhs.cend() ) res.insert( res.end(), L, lhs.cend() );
+        if( R != rhs.cend() ) res.insert( res.end(), R, rhs.cend() );
         return res;
-    }
-
-public:
-
-    Vector mergeSort( const Vector& A )
-    {
-        return go({ A.cbegin(), A.cend() });
     }
 
 };
@@ -41,9 +41,10 @@ public:
 
 int main()
 {
-    vector< int > A{ 9,5,6,3,2,8,0,4,1,7 };
-    vector< int > B{ 3,9,7,1,2,8,6,5,0,4 };
-    vector< int > C{ 3,2,9,7,1,3,7,2,8,1,6,5,0,8,0,4 };
+    const Solution< int >::Collection
+        A{ 9,5,6,3,2,8,0,4,1,7 },
+        B{ 3,9,7,1,2,8,6,5,0,4 },
+        C{ 3,2,9,7,1,3,7,2,8,1,6,5,0,8,0,4 };
 
     Solution< int > s;
     auto
