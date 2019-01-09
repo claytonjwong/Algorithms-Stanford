191b54ae2297d7fd8d47a9ef03f24452b5dcbfaf
Clayton Wong <clayton.wong@ookla.com>
2019-01-09 09:41:46 -0800

    array inversions functional implementation

---
+++ course1/array_inversions/main.cpp
@@ -0,0 +1,69 @@
+#include "input.hpp"
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+
+using namespace std;
+
+
+template< typename T >
+class Solution
+{
+public:
+
+    using Collection = vector< T >;
+
+    size_t inversions( const Collection& A )
+    {
+        return go({ A.cbegin(), A.cend() }).count;
+    }
+
+private:
+
+    struct Result
+    {
+        Collection A;
+        size_t count{ 0 };
+    };
+
+    Result go( Collection&& A )
+    {
+        if( A.size() < 2 )
+            return { A, 0 };
+        auto pivot = A.cbegin() + A.size() / 2;
+        return merge( go({ A.cbegin(), pivot }), go({ pivot, A.cend() }) );
+    }
+
+    Result merge( Result&& lhs, Result&& rhs )
+    {
+        Result result{ {}, lhs.count + rhs.count }; // left + right inversions
+        auto L = lhs.A.cbegin(), R = rhs.A.cbegin();
+        while( L != lhs.A.cend() && R != rhs.A.cend() )
+            if( *L < *R )
+                result.A.push_back( *L++ );
+            else
+                result.A.push_back( *R++ ),
+                result.count += distance( L, lhs.A.cend() ); // split inversions
+        if( L != lhs.A.cend() ) result.A.insert( result.A.end(), L, lhs.A.cend() );
+        if( R != rhs.A.cend() ) result.A.insert( result.A.end(), R, rhs.A.cend() );
+        return result;
+    }
+
+};
+
+
+int main()
+{
+    string line;
+    stringstream stream{ INPUT };
+    Solution< size_t >::Collection A;
+    for( size_t num{ 0 }; getline( stream, line ); A.push_back( num ) )
+    {
+        stringstream parser{ line };
+        parser >> num;
+    }
+    Solution< size_t > s;
+    cout << s.inversions( A ) << endl; // 2407905288
+    return 0;
+}
