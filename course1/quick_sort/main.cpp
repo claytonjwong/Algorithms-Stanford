4d17548ba2d7e3d23c3411be7b14a0f208cef496
Clayton Wong <clayton.wong@ookla.com>
2019-01-13 22:29:06 -0800

    quick sort: move random stuff to the bottom, and modify the way size is calculated to be more concise and iodimatic

--- course1/quick_sort/main.cpp
+++ course1/quick_sort/main.cpp
@@ -24,18 +24,9 @@ public:
 
 private:
 
-    using RandomDevice = random_device;
-    using Generator = mt19937;
-    using Distribution = uniform_int_distribution< int >;
-
     size_t go( Collection& A, Iter L, Iter R ) // go() sorts from [ L : R ), that is, from L (inclusive) to R (non-inclusive)
     {
         if( L >= R ) return 0;
-
-        //
-        // move random threshold value for pivot to the beginning of [ L : R ) for partitioning
-        //
-//        iter_swap( L, random( L, R ) );
 
         //
         // question #1: always use left-most [ L : R ) as pivot
@@ -50,22 +41,19 @@ private:
         //
         // question #3: use median from [ L : R ) as pivot ( note: the right-most value is at R-1 since R is non-inclusive )
         //
-        auto M = L + ceil( distance( L, R ) / 2.0 ) - 1;                 // (M)iddle
-        Collection C{ *L, *M, *(R-1) };                                  // Three pivot (C)andidates: (L)eft-most, (M)iddle, (R)ight-most element in [ L : R )
-        sort( C.begin(), C.end() );                                      // median is the middle element C[ 1 ] for the sorted array C of size 3
-        auto median = ( C[ 1 ] == *L )? L  : ( C[ 1 ] == *M )? M  : R-1; // find corresponding iterator
-        iter_swap( L, median );                                          // use median as pivot
+//        auto M = L + ceil( distance( L, R ) / 2.0 ) - 1;                 // (M)iddle
+//        Collection C{ *L, *M, *(R-1) };                                  // Three pivot (C)andidates: (L)eft-most, (M)iddle, (R)ight-most element in [ L : R )
+//        sort( C.begin(), C.end() );                                      // median is the middle element C[ 1 ] for the sorted array C of size 3
+//        auto median = ( C[ 1 ] == *L )? L  : ( C[ 1 ] == *M )? M  : R-1; // find corresponding iterator
+//        iter_swap( L, median );                                          // use median as pivot
+
+        //
+        // extra credit: move random threshold value for pivot to the beginning of [ L : R ) for partitioning
+        //
+        iter_swap( L, random( L, R ) );
 
         auto P = partition( A, L, R ); // (P)ivot
         return distance( L, R-1 ) + go( A, L, P ) + go( A, P+1, R );
-    }
-
-    Iter random( Iter L, Iter R, RandomDevice randomDevice=RandomDevice() )
-    {
-        Generator randomGenerator{ randomDevice() };
-        int size = static_cast< int >( distance( L, R ) );
-        Distribution distribution{ 0, size - 1 }; // size - 1 since R is non-inclusive
-        return L + distribution( randomGenerator );
     }
 
     Iter partition( Collection& A, Iter L, Iter R )
@@ -77,6 +65,18 @@ private:
         auto pivot{ less - 1 };
         iter_swap( L, pivot );
         return pivot;
+    }
+
+    using RandomDevice = random_device;
+    using Generator = mt19937;
+    using Distribution = uniform_int_distribution< int >;
+
+    Iter random( Iter L, Iter R, RandomDevice randomDevice=RandomDevice() )
+    {
+        Generator randomGenerator{ randomDevice() };
+        int size = distance( L, R-1 ); // R is non-inclusive
+        Distribution distribution{ 0, size };
+        return L + distribution( randomGenerator );
     }
 
 };
