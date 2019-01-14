d79d029b58897d7057127ea188fc174ff37e5db8
Clayton Wong <clayton.wong@ookla.com>
2019-01-13 22:11:10 -0800

    quick sort: functional implementation of assignment #3, to count each N-1 amount of total comparisions for each recursive call on a subarray of length N where the pivot is choosen as the left-most, right-most, and median element of each subarray

--- course1/quick_sort/main.cpp
+++ course1/quick_sort/main.cpp
@@ -1,6 +1,8 @@
+#include "input.hpp"
 #include <iostream>
 #include <vector>
 #include <random>
+#include <sstream>
 
 
 using namespace std;
@@ -16,7 +18,56 @@ public:
 
     void quickSort( Collection& A )
     {
-        go( A, A.begin(), A.end() );
+        auto count = go( A, A.begin(), A.end() );
+        cout << "Comparision count: " << count << endl;
+    }
+
+private:
+
+    using RandomDevice = random_device;
+    using Generator = mt19937;
+    using Distribution = uniform_int_distribution< int >;
+
+    size_t go( Collection& A, Iter L, Iter R ) // go() sorts from [ L : R ), that is, from L (inclusive) to R (non-inclusive)
+    {
+        if( L >= R ) return 0;
+
+        //
+        // move random threshold value for pivot to the beginning of [ L : R ) for partitioning
+        //
+//        iter_swap( L, random( L, R ) );
+
+        //
+        // question #1: always use left-most [ L : R ) as pivot
+        //
+//        iter_swap( L, L );
+
+        //
+        // question #2: always use right-most [ L : R ) as pivot ( note: this is R-1 since R is non-inclusive )
+        //
+//        iter_swap( L, R-1 );
+
+        //
+        // question #3: use median from [ L : R ) as pivot ( note: the right-most value is at R-1 since R is non-inclusive )
+        //
+        auto M = ceil( distance( L, R ) / 2.0 ) - 1;
+        Collection candidates{ *L, *(L+M), *(R-1) };
+        sort( candidates.begin(), candidates.end() );
+        auto median = ( *L == candidates[ 1 ] )? L
+                    : ( *(L+M) == candidates[ 1 ] )? L+M
+                    : R-1;
+        iter_swap( L, median );
+        
+        auto P = partition( A, L, R ); // (P)ivot
+        return distance( L, R-1 ) + go( A, L, P ) + go( A, P+1, R );
+    }
+
+    Iter random( Iter L, Iter R, RandomDevice randomDevice=RandomDevice() )
+    {
+        Generator randomGenerator{ randomDevice() };
+        int size = static_cast< int >( distance( L, R ) );
+        Distribution distribution{ 0, size - 1 }; // size - 1 since R is non-inclusive
+        return L + distribution( randomGenerator );
     }
 
     Iter partition( Collection& A, Iter L, Iter R )
@@ -30,29 +81,6 @@ public:
         return pivot;
     }
 
-private:
-
-    using RandomDevice = random_device;
-    using Generator = mt19937;
-    using Distribution = uniform_int_distribution< int >;
-
-    Iter random( Iter L, Iter R, RandomDevice randomDevice=RandomDevice() )
-    {
-        Generator randomGenerator{ randomDevice() };
-        int size = static_cast< int >( distance( L, R ) );
-        Distribution distribution{ 0, size - 1 }; // size - 1 since R is non-inclusive
-        return L + distribution( randomGenerator );
-    }
-
-    void go( Collection& A, Iter L, Iter R ) // go() sorts from [ L : R ), that is, from L (inclusive) to R (non-inclusive)
-    {
-        if( L >= R ) return;
-        iter_swap( L, random( L, R ) ); // move random threshold value for pivot to the beginning of [ L : R ) for partitioning
-        auto pivot = partition( A, L, R );
-        go( A, L, pivot );
-        go( A, pivot + 1, R );
-    }
-
 };
 
 
@@ -60,9 +88,22 @@ int main()
 {
     using Type = size_t;
     Solution< Type > s;
-    Solution< Type >::Collection A{ 9,5,6,7,2,3,1,0,4,8 };
+    Solution< Type >::Collection A;
+    stringstream stream{ INPUT };
+    for( string line; getline( stream, line ); )
+    {
+        stringstream parser{ line };
+        Type number{ 0 };
+        parser >> number;
+        A.push_back( number );
+    }
     s.quickSort( A );
     assert( is_sorted( A.cbegin(), A.cend() ) );
     copy( A.cbegin(), A.cend(), ostream_iterator< Type >( cout, " " ) );
+
+    // 1: 162085 - OK
+    // 2: 164123 - OK
+    // 3: 138382 - OK
+
     return 0;
 }
