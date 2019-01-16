6e3ec529e3743964d163e6e9a60eb8bd39ca8a98
Clayton Wong <clayton.wong@ookla.com>
2019-01-15 20:22:40 -0800

    update array inversions problem statement

--- course1/array_inversions/main.cpp
+++ course1/array_inversions/main.cpp
@@ -2,7 +2,7 @@
 
 This file contains all of the 100,000 integers between 1 and 100,000 (inclusive)
 in some order, with no integer repeated.  Your task is to compute the number of inversions
-in the file given, where the i^{th}ith row of the file indicates the i^{th}i th entry of an array.
+in the file given, where the i-th row of the file indicates the i-th entry of an array.
 Because of the large size of this array, you should implement the fast divide-and-conquer algorithm
 covered in the video lectures.
 
