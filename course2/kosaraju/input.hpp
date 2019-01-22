eabc6e5304575535feb92e919104e74ffb4c2843
Clayton Wong <clayton.wong@ookla.com>
2019-01-21 19:11:12 -0800

    course2: kosaraju failing test case #4, since topological sort doesn't find the 'sink'

---
+++ course2/kosaraju/input.hpp
@@ -0,0 +1,99 @@
+#pragma once
+
+
+#include <string>
+
+//
+// http://www.algorithmsilluminated.org/datasets/problem8.10test1.txt
+//
+const std::string TEST_CASE_1 =
+R"(1 4
+2 8
+3 6
+4 7
+5 2
+6 9
+7 1
+8 5
+8 6
+9 7
+9 3
+)";
+
+//
+// http://www.algorithmsilluminated.org/datasets/problem8.10test2.txt
+//
+const std::string TEST_CASE_2 =
+R"(1 2
+2 6
+2 3
+2 4
+3 1
+3 4
+4 5
+5 4
+6 5
+6 7
+7 6
+7 8
+8 5
+8 7
+)";
+
+//
+// http://www.algorithmsilluminated.org/datasets/problem8.10test3.txt
+//
+const std::string TEST_CASE_3 =
+R"(1 2
+2 3
+3 1
+3 4
+5 4
+6 4
+8 6
+6 7
+7 8
+)";
+
+//
+// http://www.algorithmsilluminated.org/datasets/problem8.10test4.txt
+//
+const std::string TEST_CASE_4 =
+R"(1 2
+2 3
+3 1
+3 4
+5 4
+6 4
+8 6
+6 7
+7 8
+4 3
+4 6
+)";
+
+//
+// http://www.algorithmsilluminated.org/datasets/problem8.10test5.txt
+//
+const std::string TEST_CASE_5 =
+R"(1 2
+2 3
+2 4
+2 5
+3 6
+4 5
+4 7
+5 2
+5 6
+5 7
+6 3
+6 8
+7 8
+7 10
+8 7
+9 7
+10 9
+10 11
+11 12
+12 10
+)";
--- course2/kosaraju/main.cpp
+++ course2/kosaraju/main.cpp
@@ -1,9 +1,11 @@
+#include "input.hpp"
 #include <iostream>
 #include <unordered_map>
 #include <unordered_set>
 #include <set>
 #include <vector>
 #include <queue>
+#include <sstream>
 
 
 using namespace std;
@@ -19,16 +21,16 @@ public:
     using Seen = unordered_set< Vertex >;
     using ConnectedComponents = vector< vector< Vertex > >;
     using OrderedList = vector< Vertex >;
+    using Queue = queue< Vertex >;
 
     ConnectedComponents getSCC( Graph& G, ConnectedComponents CC={}, Seen seen={} )
     {
-        auto R = reverse( G );              // (R)eversed (G)raph
+        auto R = reverse( G );           // (R)eversed (G)raph
         auto L = topo_sort( R );
-        std::reverse( L.begin(), L.end() ); // reverse topographically Ordered-(L)ist for decreasing order traversal
-        for( auto cur: L )
+        for( auto cur: L )               // L is the reverse topological order of R
             if( seen.insert( cur ).second )
                 CC.push_back( {} ),
-                dfs( CC, R, cur, seen );    // coalesce (cur)rent vertex into a new (C)onnected (C)omponent
+                dfs( CC, R, cur, seen ); // coalesce (cur)rent vertex into a new (C)onnected (C)omponent
         return CC;
     }
 
@@ -44,7 +46,8 @@ private:
 
     Graph reverse( Graph& G )
     {
-        Graph R( G.size() ); // (R)eversed (G)raph
+        Graph R( G ); // (R)eversed (G)raph: keep G's vertex keys ( pair.first ), but clear G's adjacency lists ( pair.second )
+        for_each( R.begin(), R.end(), []( auto& pair ){ pair.second={}; });
         for( auto& pair: G )
         {
             auto cur{ pair.first };
@@ -54,26 +57,26 @@ private:
         return R;
     }
 
-    OrderedList topo_sort( Graph& G, Seen seen={} )
+    OrderedList topo_sort( Graph& G, Seen visiting={}, Seen visited={} )
     {
-        auto N{ G.size() };
-        OrderedList L( N + 1 );
-        for( auto& pair: G )
-        {
-            auto cur{ pair.first };
-            if( seen.insert( cur ).second )
-                go( L, G, cur, N, seen );
-        }
-        return { L.cbegin() + 1, L.cend() }; // return buckets as 0-based index of [ 1 : N+1 )
-    }
+        OrderedList L;
 
-    void go( OrderedList& L, Graph& G, Vertex cur, // (cur)rent vertex at the top of the callstack
-             size_t& N, Seen& seen )
-    {
-        for( auto adj: G[ cur ] )              // (adj)acent neighbor vertices of the (G)raph's (cur)rent vertex
-            if( seen.insert( adj ).second )    // if this is the first time the (adj)acent neighbor vertex has been seen
-                go( L, G, adj, N, seen );      // go further process (adj)acent neighbor vertex
-        L[ N-- ] = cur;                        // update ordered-(L)ist entries in reverse order as the callstack returns from [ N : 1 ]
+        // TODO: write working TopoSort implementation on directed cyclic graphs,
+        //       5 should be the first in the reverse ordered list, because its a "sink"
+/*
+
+            1 ----> 2 ----> 3
+              <---- | <----
+                    |
+                    V
+                    5
+
+*/
+        // TODO: check pseudocode for TopoSort ( pg 50 ) and Kosaraju ( pg 62 ) and re-write code verbatim?
+
+        // TODO: if above doesn't work, maybe look for functional implementations to see why test case #5 isn't topologically sorted as expected
+
+        return L;
     }
 
 };
@@ -84,7 +87,6 @@ int main()
     // graph from figure 8.8, page 35 of Algorithms Illuminated: Part 2
     //
     Solution::Graph G{
-        { 0, {} },          // note: include disjoint vertex 0 for simplicity of 0-based indexing
         { 1, { 3 } },
         { 2, { 4, 10 } },
         { 3, { 5, 11 } },
@@ -99,6 +101,29 @@ int main()
     };
     Solution s;
     auto scc = s.getSCC( G );
+
+    for( auto& test: { TEST_CASE_4 } ) //TEST_CASE_1, TEST_CASE_2, TEST_CASE_3, TEST_CASE_4, TEST_CASE_5
+    {
+        G.clear();
+        auto u{ 0 }, v{ 0 };
+        stringstream input{ test };
+        for( string line; getline( input, line ); G[ u ].push_back( v ) )
+        {
+            stringstream parser{ line }; parser >> u >> v;
+            if( G.find( u ) == G.end() )
+                G[ u ] = {};
+        }
+        auto result = s.getSCC( G );
+        auto index{ 0 };
+        for( auto& component: result )
+        {
+            cout << index++ << ": ";
+            for( auto& vertex: component )
+                cout << vertex << " ";
+            cout << endl;
+        }
+        cout << endl;
+    }
 
     return 0;
 }
