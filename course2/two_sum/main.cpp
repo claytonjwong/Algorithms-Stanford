ca74ba536aa88f444a8b3951607caeb6c41f2bcf
Clayton Wong <clayton.wong@ookla.com>
2019-01-31 22:12:50 -0800

    add problem description and output for the fast/slow answer durations

--- course2/two_sum/main.cpp
+++ course2/two_sum/main.cpp
@@ -1,3 +1,21 @@
+/*
+
+The goal of this problem is to implement a variant of the 2-SUM algorithm covered in this week's lectures.
+The file contains 1 million integers, both positive and negative (there might be some repetitions!).
+This is your array of integers, with the i^th row of the file specifying the i^th entry of the array.
+
+Your task is to compute the number of target values t in the interval [-10000,10000] (inclusive)
+such that there are distinct numbers x,yx,y in the input file that satisfy x+y=t.
+(NOTE: ensuring distinctness requires a one-line addition to the algorithm from lecture.)
+
+Write your numeric answer (an integer between 0 and 20001) in the space provided.
+
+OPTIONAL CHALLENGE: If this problem is too easy for you, try implementing your own hash table for it.
+For example, you could compare performance under the chaining and open addressing approaches to resolving collisions.
+
+*/
+
+
 #include <iostream>
 #include <sstream>
 #include <fstream>
@@ -78,14 +96,22 @@ int main()
     auto fastStart = Time::now();
     auto fastResult = fast.count2SumRange( "input.txt", -10000, 10000 );
     auto fastFinish = Time::now();
-    cout << "answer: " << fastResult << endl
-         << "duration: " << duration_cast< seconds >( fastFinish - fastStart ).count() << " seconds " << endl;
+    cout << "fast answer: " << fastResult << endl
+         << "fast duration: " << duration_cast< seconds >( fastFinish - fastStart ).count() << " seconds " << endl << endl;
 
     SlowSolution< Type > slow;
     auto slowStart = Time::now();
     auto slowResult = slow.count2SumRange( "input.txt", -10000, 10000 );
     auto slowFinish = Time::now();
-    cout << "answer: " << slowResult << endl
-         << "duration: " << duration_cast< seconds >( slowFinish - slowStart ).count() << " seconds " << endl;
+    cout << "slow answer: " << slowResult << endl
+         << "slow duration: " << duration_cast< seconds >( slowFinish - slowStart ).count() << " seconds " << endl;
+
+//    fast answer: 427
+//    fast duration: 3 seconds
+//
+//    slow answer: 427
+//    slow duration: 3273 seconds
+
+
     return 0;
 }
