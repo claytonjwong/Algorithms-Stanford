79796e37cec7f931310f0f8a9315ff8689b87155
Clayton Wong <clayton.wong@ookla.com>
2019-02-02 12:03:24 -0800

    Prim's MST first draft ( not working )

--- course3/prim_mst/main.cpp
+++ course3/prim_mst/main.cpp
@@ -29,14 +29,87 @@ IMPLEMENTATION NOTES: This graph is small enough that the straightforward O(mn) 
 
 */
 
+#include "input.hpp"
 #include <iostream>
+#include <sstream>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+#include <set>
 
 
 using namespace std;
 
 
+template< typename Type >
+class Solution
+{
+public:
+
+    using Vertex = Type;
+    using Cost = Type;
+    struct Edge
+    {
+        Vertex tail{ 0 }, head{ 0 };
+        bool operator==( const Edge& rhs ) const { return tail == rhs.tail && head == rhs.head; }
+    };
+    struct Hash{ size_t operator()( const Edge& e ) const { return e.tail << 16 + e.head; } };
+    using Edges = unordered_map< Edge, Cost, Hash >;
+    using AdjacencyList = unordered_set< Vertex >;
+    using Graph = unordered_map< Vertex, AdjacencyList >;
+    using Tree = unordered_set< Vertex >;
+    using EdgeCost = pair< Edge, Cost >;
+
+    Type costMST( Graph& G, Edges& E, Vertex start, Type cost=0 )
+    {
+        auto Compare = []( const EdgeCost& lhs, const EdgeCost& rhs ){ return lhs.second < rhs.second; };
+        set< EdgeCost, decltype( Compare )> edges( {}, Compare );
+        for( Tree tree{ start }; tree.size() < G.size(); )
+        {
+            for( auto vertex{ tree.begin() }; vertex != tree.end(); ++vertex )
+                for( auto& adj: G[ *vertex ] )
+                    for( auto edgeCost = E.find({ *vertex, adj });
+                         edgeCost != E.end() && tree.find( adj ) == tree.end();
+                         edges.insert({ edgeCost->first, edgeCost->second }), // executed at-most once ( if the edgeCost is found )
+                         edgeCost = E.end() );
+            auto min = edges.begin();
+            auto minEdge = min->first;
+            auto minCost = min->second;
+            auto cur{ minEdge.tail }, // the first edge has minimal cost from (cur)rrent vertex ( included in the MST )
+                 adj{ minEdge.head }; // to the (adj)acent vertex ( to be included in the MST )
+            tree.insert( adj );
+            cost += minCost;
+        }
+        return cost;
+    }
+
+};
+
+
 int main()
 {
-
+    using Type = int;
+    Solution< Type > s;
+    for( auto& input: { Lecture::Input, Assignment::Input } ) // assume input graph is undirected and connected
+    {
+        Solution< Type >::Graph G;
+        Solution< Type >::Edges E;
+        istringstream stream{ input };
+        for( string line; getline( stream, line ); )
+        {
+            stringstream parser{ line };
+            Type u{ 0 }, v{ 0 }, cost{ 0 };
+            parser >> u >> v >> cost;
+            if( G.find( u ) == G.end() ) G[ u ] = {};
+            if( G.find( v ) == G.end() ) G[ v ] = {};
+            G[ u ].insert( v );
+            G[ v ].insert( u );
+            E.insert({ {u,v}, cost });
+            E.insert({ {v,u}, cost });
+        }
+        auto start{ G.begin()->first }; // arbitrary start vertex is irrelevant
+        auto answer = s.costMST( G, E, start );
+        cout << "answer: " << answer << endl;
+    }
     return 0;
 }
