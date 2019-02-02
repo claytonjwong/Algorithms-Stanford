3b251bdaebb076caf07b2946da1d51c537b8fa51
Clayton Wong <clayton.wong@ookla.com>
2019-02-01 17:27:23 -0800

    minor edit to problem description

--- course2/two_sum/main.cpp
+++ course2/two_sum/main.cpp
@@ -5,7 +5,7 @@ The file contains 1 million integers, both positive and negative (there might be
 This is your array of integers, with the i^th row of the file specifying the i^th entry of the array.
 
 Your task is to compute the number of target values t in the interval [-10000,10000] (inclusive)
-such that there are distinct numbers x,yx,y in the input file that satisfy x+y=t.
+such that there are distinct numbers x,y in the input file that satisfy x+y=t.
 (NOTE: ensuring distinctness requires a one-line addition to the algorithm from lecture.)
 
 Write your numeric answer (an integer between 0 and 20001) in the space provided.
