1b9e7c032c5270ca09841fe2dfe87f5e8df09cd0
Clayton Wong <clayton.wong@ookla.com>
2019-02-02 16:56:52 -0800

    naive karatsuba multplication: simply calculate log/pow of 10 recursively, and copy/paste output at the bottom of main for quick reference

--- course1/karatsuba_multiplication/main.cpp
+++ course1/karatsuba_multiplication/main.cpp
@@ -1,35 +1,32 @@
 #include <iostream>
-#include <cmath>
 
 
 using namespace std;
 
 
-template< typename T >
-T POW_10( const T N ){ return static_cast< T >( pow( 10, N ) ); }
-
-template< typename T >
-T LOG_10( const T X ){ auto N{ 1 }; while( POW_10( N ) <= X ){ ++N; }; return N; }
-
-
-template< typename T >
+template< typename Type >
 class Solution
 {
-    T go( const T x, const T y )
+public:
+
+    Type multiply( const Type x, const Type y )
+    {
+        return go( x,y );
+    }
+
+private:
+
+    Type POW_10( Type N ){ return( N == 0 )? 1 : 10 * POW_10( N - 1 ); }
+    Type LOG_10( Type N ){ return( N == 0 )? 0 : 1 + LOG_10( N / 10 ); }
+
+    Type go( const Type x, const Type y )
     {
         if( x < 10 || y < 10 ) return x * y;
         auto i{ LOG_10(x) }, j{ LOG_10(y) }, N{ min(i,j) }, p{ POW_10(N/2) }, // (p)ivot
-             a{ x / p }, b{ x % p },
-             c{ y / p }, d{ y % p },
-             u{ go(a,c) }, v{ go(a+b,c+d) }, w{ go(b,d) };
+            a{ x / p }, b{ x % p },
+            c{ y / p }, d{ y % p },
+            u{ go(a,c) }, v{ go(a+b,c+d) }, w{ go(b,d) };
         return( u * POW_10(N)  +  (v-u-w) * POW_10(N/2)  +  w );
-    }
-
-public:
-
-    T multiply( const T x, const T y )
-    {
-        return go( x,y );
     }
 
 };
@@ -44,5 +41,11 @@ int main()
     cout << s.multiply( -11, 11 ) << endl;
     cout << s.multiply( -69, -31 ) << endl;
 
+//    21
+//    400
+//    7006652
+//    -121
+//    2139
+    
     return 0;
 }
