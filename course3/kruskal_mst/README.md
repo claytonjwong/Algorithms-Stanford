15aeb195c4a367698d53edaa99f1a051b924f0b2
Clayton Wong <clayton.wong@ookla.com>
2019-02-09 20:44:56 -0800

    kruskal: add README, refactor main to code re-use and readability

---
+++ course3/kruskal_mst/README.md
@@ -0,0 +1,61 @@
+<h1 id="Kruskal">Kruskal</h1>
+<a href="https://en.wikipedia.org/wiki/Kruskal%27s_algorithm">https://en.wikipedia.org/wiki/Kruskal%27s_algorithm</a>
+<h2>Contents</h2>
+<ul>
+  <li>
+      <a href="#slides">Lecture Slides</a>
+  </li>
+  <li>
+    <a href="#solution">Solution</a>
+  </li>
+  <li>
+    <a href="#build">Build Instructions</a>
+  </li>
+  <li>
+    <a href="#dependencies">Dependencies</a>
+  </li>
+</ul>
+
+<h2 id="slides">Lecture Slides</h2>
+<img src="https://github.com/claytonjwong/Algorithms-Stanford/blob/master/course3/kruskal_mst/documentation/kruskal_01.png" />
+<img src="https://github.com/claytonjwong/Algorithms-Stanford/blob/master/course3/kruskal_mst/documentation/kruskal_02.png" />
+<img src="https://github.com/claytonjwong/Algorithms-Stanford/blob/master/course3/kruskal_mst/documentation/kruskal_03.png" />
+<img src="https://github.com/claytonjwong/Algorithms-Stanford/blob/master/course3/kruskal_mst/documentation/kruskal_04.png" />
+<img src="https://github.com/claytonjwong/Algorithms-Stanford/blob/master/course3/kruskal_mst/documentation/kruskal_05.png" />
+<img src="https://github.com/claytonjwong/Algorithms-Stanford/blob/master/course3/kruskal_mst/documentation/kruskal_06.png" />
+<img src="https://github.com/claytonjwong/Algorithms-Stanford/blob/master/course3/kruskal_mst/documentation/kruskal_07.png" />
+<img src="https://github.com/claytonjwong/Algorithms-Stanford/blob/master/course3/kruskal_mst/documentation/kruskal_08.png" />
+<img src="https://github.com/claytonjwong/Algorithms-Stanford/blob/master/course3/kruskal_mst/documentation/kruskal_09.png" />
+<img src="https://github.com/claytonjwong/Algorithms-Stanford/blob/master/course3/kruskal_mst/documentation/kruskal_10.png" />
+<img src="https://github.com/claytonjwong/Algorithms-Stanford/blob/master/course3/kruskal_mst/documentation/kruskal_11.png" />
+<img src="https://github.com/claytonjwong/Algorithms-Stanford/blob/master/course3/kruskal_mst/documentation/kruskal_12.png" />
+<img src="https://github.com/claytonjwong/Algorithms-Stanford/blob/master/course3/kruskal_mst/documentation/kruskal_13.png" />
+<img src="https://github.com/claytonjwong/Algorithms-Stanford/blob/master/course3/kruskal_mst/documentation/kruskal_14.png" />
+<img src="https://github.com/claytonjwong/Algorithms-Stanford/blob/master/course3/kruskal_mst/documentation/kruskal_15.png" />
+<img src="https://github.com/claytonjwong/Algorithms-Stanford/blob/master/course3/kruskal_mst/documentation/kruskal_16.png" />
+<img src="https://github.com/claytonjwong/Algorithms-Stanford/blob/master/course3/kruskal_mst/documentation/kruskal_17.png" />
+<img src="https://github.com/claytonjwong/Algorithms-Stanford/blob/master/course3/kruskal_mst/documentation/kruskal_18.png" />
+
+<h2 id="solution">Solution</h2>
+<pre>
+
+
+
+</pre>
+
+<h2 id="build">Build Instructions</h2>
+<p>Use <code>cmake</code> to build this project:</p>
+
+<pre>
+    cmake --build cmake-build-debug --target knapsack -- -j 4
+</pre>
+
+<h2 id="dependencies">Dependencies</h2>
+<ul>
+  <li>
+    <a href="https://cmake.org/">cmake.org</a>
+  </li>
+</ul>
+
+</body>
+</html>
--- course3/kruskal_mst/main.cpp
+++ course3/kruskal_mst/main.cpp
@@ -1,3 +1,36 @@
+/*
+
+In this programming problem you'll code up Prim's minimum spanning tree algorithm.
+
+Download the text file below.
+
+This file describes an undirected graph with integer edge costs. It has the format
+
+[number_of_nodes] [number_of_edges]
+
+[one_node_of_edge_1] [other_node_of_edge_1] [edge_1_cost]
+
+[one_node_of_edge_2] [other_node_of_edge_2] [edge_2_cost]
+
+...
+
+For example, the third line of the file is "2 3 -8874",
+indicating that there is an edge connecting vertex #2 and vertex #3 that has cost -8874.
+
+You should NOT assume that edge costs are positive, nor should you assume that they are distinct.
+
+Your task is to run Prim's minimum spanning tree algorithm on this graph. You should report the overall cost
+of a minimum spanning tree --- an integer, which may or may not be negative --- in the box below.
+
+IMPLEMENTATION NOTES: This graph is small enough that the straightforward O(mn) time implementation
+of Prim's algorithm should work fine. OPTIONAL: For those of you seeking an additional challenge,
+try implementing a heap-based version. The simpler approach, which should already give you a healthy speed-up,
+is to maintain relevant edges in a heap (with keys = edge costs). The superior approach stores the unprocessed vertices
+in the heap, as described in lecture. Note this requires a heap that supports deletions,
+and you'll probably need to maintain some kind of mapping between vertices and their positions in the heap.
+
+*/
+
 #include "input.hpp"
 #include <iostream>
 #include <sstream>
@@ -9,6 +42,7 @@
 
 
 using namespace std;
+using Type = int;
 
 
 template< typename Type >
@@ -71,40 +105,49 @@ private:
                 lead[ lu ] = lv; // arbitrary lead choice
         }
 
-        Vertex Find( Vertex v ){ return( lead[ v ] == v )? v : lead[ v ] = Find( lead[ v ] ); }
+        Vertex Find( Vertex v )
+        {
+            if( v == lead[ v ] )
+                return v;
+            else
+                return lead[ v ] = Find( lead[ v ] );
+        }
     };
 
 };
 
 
+Type costMST( const string& input )
+{
+    Solution< Type > s;
+    Solution< Type >::Graph G;
+    Solution< Type >::Edges E;
+    istringstream stream{ input };
+    for( string line; getline( stream, line ); )
+    {
+        stringstream parser{ line };
+        Type u{ 0 }, v{ 0 }, cost{ 0 };
+        parser >> u >> v >> cost;
+        if( G.find( u ) == G.end() ) G[ u ] = {};
+        if( G.find( v ) == G.end() ) G[ v ] = {};
+        G[ u ].insert( v );
+        G[ v ].insert( u );
+        E.insert({ {u,v}, cost });
+        E.insert({ {v,u}, cost });
+    }
+    return s.costMST( G, E );
+}
+
+
 int main()
 {
-    using Type = int;
-    Solution< Type > s;
-    for( auto& input: { Lecture::Input, Assignment::Input } ) // assume input graph is undirected and connected
-    {
-        Solution< Type >::Graph G;
-        Solution< Type >::Edges E;
-        istringstream stream{ input };
-        for( string line; getline( stream, line ); )
-        {
-            stringstream parser{ line };
-            Type u{ 0 }, v{ 0 }, cost{ 0 };
-            parser >> u >> v >> cost;
-            if( G.find( u ) == G.end() ) G[ u ] = {};
-            if( G.find( v ) == G.end() ) G[ v ] = {};
-            G[ u ].insert( v );
-            G[ v ].insert( u );
-            E.insert({ {u,v}, cost });
-            E.insert({ {v,u}, cost });
-        }
-        auto answer = s.costMST( G, E );
-        cout << "answer: " << answer << endl << endl;
-    }
 
-//    answer: 7
+        cout << "lecture answer: " << costMST( Lecture::Input ) << endl << endl
+             << "assignment answer: " << costMST( Assignment::Input ) << endl << endl;
 
-//    answer: -3612829
+//    lecture answer: 7
+
+//    assignment answer: -3612829
 
     return 0;
 }
