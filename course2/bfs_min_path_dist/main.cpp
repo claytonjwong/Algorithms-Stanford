5bcedde7842a9e292ae16abaadff16c383b14425
Clayton Wong <clayton.wong@ookla.com>
2019-02-12 19:34:00 -0800

    min path dist: update README

--- course2/bfs_min_path_dist/main.cpp
+++ course2/bfs_min_path_dist/main.cpp
@@ -1,3 +1,12 @@
+/**
+ *
+ * Modern C++ implementation of Breadth First Search algorithm to find the single-source minimum path distances
+ *
+ * (c) Copyright 2019 Clayton J. Wong ( http://www.claytonjwong.com )
+ *
+ **/
+
+
 #include <iostream>
 #include <vector>
 #include <unordered_map>
