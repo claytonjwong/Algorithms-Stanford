48b69aeaf896777a3f74ec00478add50fbcc7e99
Clayton Wong <clayton.wong@ookla.com>
2019-02-15 17:35:31 -0800

    floyd warshall: functional implementation ( bottom-up DP )

--- course4/floyd_warshall/main.cpp
+++ course4/floyd_warshall/main.cpp
@@ -8,28 +8,153 @@
 
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <chrono>
+
+
+#define ASSIGNMENT_INPUT 69 // comment this line to execute tests on the large optional input
 
 
 using namespace std;
-
-
-namespace TopDown
+using namespace std::chrono;
+using InputFiles = vector< string >;
+#ifdef ASSIGNMENT_INPUT
+const auto N{ 1000 };
+const InputFiles inputFiles{ "g1.txt", "g2.txt", "g3.txt" };
+#else
+const auto N{ 20000 };
+const InputFiles inputFiles{ "large.txt" };
+#endif
+using Integer = long long;
+using Vertex = Integer;
+using Cost = Integer;
+using VI = vector< Cost >;
+using VVI = vector< VI >;
+using VVVI = vector< VVI >;
+static const Cost INF = numeric_limits< Cost >::max();
+struct Edge
 {
+    Vertex u{ 0 }, v{ 0 };
+    bool operator==( const Edge& rhs ) const { return u == rhs.u && v == rhs.v; }
+};
+struct Hash{ Cost operator()( const Edge& e ) const { return ( N+1 ) * e.u + e.v; } };
+using Edges = unordered_map< Edge, Cost, Hash >;
+using Answer = pair< VVVI, bool >;
 
 
-} // namespace TopDown
-
-
-namespace BottomUp
+Edges readInput( const string& filename, Edges edges={} )
 {
+    fstream stream{ filename };
+    for( string line; getline( stream, line ); )
+    {
+        stringstream parser{ line };
+        Vertex i,j; Cost cost;
+        parser >> i >> j >> cost;
+        Edge edge{ i,j };
+        edges.insert({ edge, cost }); // i -> j
+    }
+    return edges;
+}
 
 
-} // namespace BottomUp
+class Solution
+{
+public:
 
+    Answer getShortestPaths( Edges& E )
+    {
+        VVVI dp( N+1, VVI( N+1, VI( N+1, INF )));                       // 1-based indexing from [1:N]
+        for( auto i{ 1 }; i <= N; ++i ) for( auto j{ 1 }; j <= N; ++j ) // for each i,j: base cases for k == 1 ( k is non-inclusive, so Vertex 1 is NOT considered here )
+        {
+            Edge edge{ i,j };
+            auto it = E.find( edge );
+            auto cost = ( it != E.end() )? it->second : INF;
+            dp[ i ][ j ][ 1 ] = ( i == j )? 0 : cost;
+        }
+        for( auto k{ 2 }; k <= N; ++k ) for( auto i{ 2 }; i <= N; ++i ) for( auto j{ 2 }; j <= N; ++j ) // for each i,j,
+        {
+            // Note: let (1...k-1) denote a path which is only comprised of vertices [1:k-1], that is 1 inclusive to k-1 inclusive
+            //       this does NOT mean that all of these vertices are included in this path, but these vertices are the only potential candidates
+            //       which may potentially be included in the path ( this is a fundamental concept of this algorithm to create overlapping subproblems! )
+            auto pre = dp[ i ][ j ][ k-1 ],                         // (pre)vious cost of path i -> (1...k-1) -> j  ( without k )
+                 Cik = dp[ i ][ k ][ k-1 ],                         // cost of path i -> (1...k-1) -> k
+                 Ckj = dp[ k ][ j ][ k-1 ],                         // cost of path k -> (1...k-1) -> j
+                 alt = ( Cik < INF && Ckj < INF )? Cik + Ckj : INF; // (alt)ernative cost of path i -> (1...k-1) -> k -> (1...k-1) -> j
+            dp[ i ][ j ][ k ] = min( pre, alt );
+        }
+        return{ dp, hasCycle( dp ) };
+    }
+
+    Cost shortestPath( const VVVI& A )
+    {
+        if( hasCycle( A ) )
+            return INF;
+        auto cost{ INF };
+        for( auto i{ 1 }; i <= N; ++i )
+            for( auto j{ 1 }; j <= N; ++j )
+                if( cost > A[ i ][ j ][ N ] )
+                    cost = A[ i ][ j ][ N ];
+        return cost;
+    }
+
+private:
+
+    bool hasCycle( const VVVI& A )
+    {
+        for( auto i{ 1 }; i <= N; ++i )
+            if( A[ i ][ i ][ N ] < 0 )
+                return true;
+        return false;
+    }
+
+}; // class Solution
+
+
+
+string test( const string& inputFile )
+{
+    ostringstream outStream;
+    Solution solution;
+    auto E = readInput( inputFile );
+    auto[ A, hasCycle ] = solution.getShortestPaths( E );
+    outStream << inputFile << ": ";
+    if( hasCycle )
+        outStream << "has a cycle";
+    else
+        outStream << "has shortest path " << solution.shortestPath( A );
+    return outStream.str();
+}
+
+using Minute = duration< long long, minutes >;
+using Minutes = vector< Minute >;
+using Time = std::chrono::high_resolution_clock;
 
 int main()
 {
 
+    for( auto& inputFile: inputFiles )
+    {
+        string output;
+        auto start = Time::now();
+        {
+            output = test( inputFile );
+        }
+        auto finish = Time::now();
+        cout << output << " ( execution time: "
+             << duration_cast< minutes >( finish - start ).count()
+             << " minutes )" << endl << endl;
+    }
+
+//    g1.txt: has a cycle ( execution time: 3 minutes )
+//
+//    g2.txt: has a cycle ( execution time: 3 minutes )
+//
+//    g3.txt: has shortest path -19 ( execution time: 3 minutes )
 
     return 0;
 }
