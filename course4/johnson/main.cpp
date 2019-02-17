188c242178fb9b5d60d61f8ea3384a633927b95e
Clayton Wong <clayton.wong@ookla.com>
2019-02-17 15:14:59 -0800

    johnson: implement memory optimization for bellman-ford portion

--- course4/johnson/main.cpp
+++ course4/johnson/main.cpp
@@ -103,36 +103,35 @@ class BellmanFord
 {
 public:
 
-    using Answer = pair< VVI, bool >;
+    using Answer = pair< VI, bool >;
 
     static Answer getShortestPaths( Graph& G, Edges& E, Vertex start )
     {
-        VVI dp( N+1, VI( N+1, INF )); // 1-based indexing
-        dp[ 0 ][ start ] = 0;
-        for( auto i{ 1 }; i <= N; ++i )
+        VI pre( N+1, INF ), cur( pre );
+        pre[ start ] = 0;
+        for( auto i{ 1 }; i <= N; ++i, swap( pre, cur ) )
             for( auto& pair: G )
             {
                 auto v{ pair.first };
-                dp[ i ][ v ] = dp[ i-1 ][ v ]; // (pre)vious path or minimum (alt)erative path to v through w + cost of edge wv
+                cur[ v ] = pre[ v ]; // (pre)vious path or minimum (alt)erative path to v through w + cost of edge wv
                 for( auto w: G[ v ] ) // w -> v
                 {
                     Edge wv{ w,v };
-                    Cost Cw = dp[ i-1 ][ w ],
+                    Cost Cw = pre[ w ],
                          Cwv = E[ wv ],
                          alt = ( Cw < INF )? Cw + Cwv : INF;
-                    if( dp[ i ][ v ] > alt )
-                        dp[ i ][ v ] = alt;
+                    if( cur[ v ] > alt )
+                        cur[ v ] = alt;
                 }
             }
-        auto hasCycle = BellmanFord::hasCycle( G, E, dp );
-        return{ dp, hasCycle };
+        auto hasCycle = BellmanFord::hasCycle( G, E, cur );
+        return{ cur, hasCycle };
     }
 
 private:
 
-    static bool hasCycle( Graph& G, Edges& E, VVI& dp )
+    static bool hasCycle( Graph& G, Edges& E, VI& P )
     {
-        auto P{ dp.back() };
         for( auto& e: E )
         {
             auto edge{ e.first };
@@ -202,14 +201,13 @@ public:
         Vertex source{ 0 };
         auto[ Gs, Es ] = Johnson::addSourceVertex( G, E, source );
         Gs = reverse( Gs ); // Gs was generated with outgoing adjacency lists, reverse Gs for incoming adjacency lists needed by Bellman-Ford
-        auto[ A, hasCycle ] = BellmanFord::getShortestPaths( Gs, Es, source );
+        auto[ P, hasCycle ] = BellmanFord::getShortestPaths( Gs, Es, source );
         if( hasCycle )
             return{ {}, true };
         //
         // re-weight edges using the (P)aths found from runnning Bellman-Ford
         // on the augmented graph Gs ( G with new vertex 'source' )
         //
-        auto P{ A.back() };
         for( auto& pair: E )
         {
             auto edge{ pair.first };
