ea18e430e9b40a867bfeb0d7f7a4dbaa7cc547e2
Clayton Wong <clayton.wong@ookla.com>
2019-02-22 19:33:27 -0800

    minor updates to python solution to use pow10() / log10() instead of pow() / log(), since those seem to be keywords and show up a different color

--- course1/karatsuba_multiplication/main.py
+++ course1/karatsuba_multiplication/main.py
@@ -6,23 +6,23 @@
 #
 ##
 
-def pow( N ):
+def pow10( N ):
     if N == 0:
         return 1
-    return 10 * pow( N-1 )
+    return 10 * pow10( N-1 )
 
-def log( N ):
+def log10( N ):
     if N == 0:
         return 0
-    return 1 + log( N/10 )
+    return 1 + log10( N/10 )
 
 def go( x, y ):
     if x < 10 or y < 10:
         return x * y
-    i = log( x )       # number of digits in x
-    j = log( y )       # number of digits in y
+    i = log10( x )     # number of digits in x
+    j = log10( y )     # number of digits in y
     N = min( i, j )    # minimum number of digits between x, y
-    p = pow( N/2 )     # pivot position 
+    p = pow10( N/2 )   # pivot position 
     a = x / p          # a is the first half of x
     b = x % p          # b is the second half of x
     c = y / p          # c is the first half of y
@@ -30,7 +30,7 @@ def go( x, y ):
     u = go( a, c )
     v = go( a+b, c+d )
     w = go( b, d )
-    return u * pow( N ) + ( v -u -w ) * pow( N/2 ) + w
+    return u * pow10( N ) + ( v -u -w ) * pow10( N/2 ) + w
 
 def multiply( x, y ):
     return go( x, y )
