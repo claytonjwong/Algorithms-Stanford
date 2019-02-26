ac044b5a5e34834fb19e22696b84e87984912fd9
Clayton Wong <clayton.wong@ookla.com>
2019-02-25 20:35:06 -0800

    karger min cut: update head/tail to u/v and add comments for better understanding

--- course1/karger_min_cut/main.cpp
+++ course1/karger_min_cut/main.cpp
@@ -9,12 +9,11 @@
 
 #include "input.hpp"
 #include <iostream>
+#include <sstream>
 #include <vector>
 #include <set>
-#include <map>
 #include <unordered_map>
 #include <random>
-#include <sstream>
 
 
 using namespace std;
@@ -24,10 +23,10 @@ class Solution
 {
 public:
 
-    using Vertex = int;
+    using Vertex = size_t;
     using AdjacencyList = multiset< Vertex >;
     using Graph = unordered_map< Vertex, AdjacencyList >;
-    struct Edge{ Vertex tail{ 0 }, head{ 0 }; };
+    struct Edge{ Vertex u{ 0 }, v{ 0 }; };
 
     int getMinCut( Graph G ) // assume (G)raph is connected undirected with at least one edge and two vertices
     {
@@ -40,7 +39,7 @@ private:
 
     using RandomDevice = random_device;
     using Generator = mt19937;
-    using Distribution = uniform_int_distribution< int >;
+    using Distribution = uniform_int_distribution< Vertex >;
 
     int randomVertex( Vertex begin, Vertex end, RandomDevice randomDevice=RandomDevice() )
     {
@@ -51,26 +50,24 @@ private:
 
     Edge randomEdge( Graph& G )
     {
-        auto N = static_cast< Vertex >( G.size() ),
-             tail = next( G.cbegin(), randomVertex( 0, N ) )->first;
-        auto M = static_cast< Vertex >( G[ tail ].size() ),
-             head = *next( G[ tail ].cbegin(), randomVertex( 0, M ) );
-        return { tail, head };
+        auto N{ G.size() }, u{ next( G.cbegin(), randomVertex( 0, N ))->first };     // random u from |V| ( the set of all vertices )
+        auto M{ G[ u ].size() }, v{ *next( G[ u ].cbegin(), randomVertex( 0, M )) }; // random v from the set of vertices adjacent to u
+        return { u,v };
     }
 
-    void contractEdge( Graph& G, const Edge& edge )
+    void contractEdge( Graph& G, const Edge& edge ) // coalesce u into v, then delete u
     {
-        auto head{ edge.head }, tail{ edge.tail };
-        for( auto& adj: G[ tail ] )
+        auto v{ edge.v }, u{ edge.u };
+        for( auto& adj: G[ u ] )  // contract edge{ u -> v }: create edges from v based on u's (adj)acent vertices  and erase each vertex (adj)acent to u
         {
-            if( adj == head )
-                continue; // avoid adding self-loop when coalescing tail's adj(acent) vertices into head
-            G[ head ].insert( adj );
-            G[ adj ].insert( head );
-            G[ adj ].erase( tail );
+            if( adj == v )
+                continue;         // avoid adding self-loop when coalescing u's adj(acent) vertices into v
+            G[ v ].insert( adj ); // edge{ v -> adj }: each (adj)acent vertex of u is contracted as an (adj)acent vertex to v
+            G[ adj ].insert( v ); // edge{ adj -> v }: since the graph is undirected, also add an (adj)acency in the opposite direction
+            G[ adj ].erase( u );  // since the graph is undirected, erase u from each of u's (adj)acent vertices
         }
-        G[ head ].erase( tail );
-        G.erase( tail );
+        G.erase( u ); // erase all edges{ u -> adj } and edge{ u -> v }: performed all-at-once VS piecemeal removal of edge{ adj -> u } above
+        G[ v ].erase( u ); // since the graph is undirected, also remove v -> u
     }
 
 };
@@ -100,7 +97,7 @@ int main()
     }
 
     auto minCut = numeric_limits< int >::max();
-    for( auto N{ 100 }; N--; minCut = min( minCut, s.getMinCut( G ) ) );
+    for( auto N{ 100 }; N--; minCut = min( minCut, s.getMinCut( G ) ) ); // run min cut algo an arbitrary amount of times
     cout << "minCut: " << minCut << endl;
 
     // minCut: 17
