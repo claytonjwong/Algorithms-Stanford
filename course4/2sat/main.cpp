a5679b074ecf2607267cad30e75a92fa738d1da3
Clayton Wong <clayton.wong@ookla.com>
2019-02-25 20:34:06 -0800

    2sat: possible solution, also include lecture input, and update documentation

--- course4/2sat/main.cpp
+++ course4/2sat/main.cpp
@@ -15,10 +15,12 @@
 #include <random>
 #include <unordered_map>
 #include <unordered_set>
+#include <algorithm>
 
 
 using namespace std;
 using Type = int;
+constexpr auto INF = numeric_limits< Type >::max();
 struct Literal
 {
     Type value{ 0 };
@@ -30,69 +32,81 @@ struct Clause
     bool isSatisfied(){ return( first.assignment == first.constraint ) || ( second.assignment == second.constraint ); }
 };
 using ClauseList = vector< Clause >;
-using HClauseList = vector< shared_ptr< Clause > >;
-using ValueClauseMap = unordered_map< Type, HClauseList >;
-using ClauseSet = unordered_set< Clause >;
 
 
-class Solution
+namespace Papadimitriou
 {
-public:
-
-    bool isSatisfiable( ClauseList& clauseList )
+    class Solution
     {
-        auto N{ clauseList.size() };
-        auto logN{ 0 }; for( auto n{ N }; n > 1; n >>= 1, ++logN );
-        while( logN-- > 0 )
+    public:
+
+        bool isSatisfiable( ClauseList& clauseList )
         {
-            randomInitialAssignment( clauseList );
-            for( auto i{ 0 }; i < 2 * N * N; ++i )
+            auto N{ clauseList.size() };
+            auto logN{ 0 }; for( auto n{ N }; n > 1; n >>= 1, ++logN );
+            while( logN-- > 0 )
             {
-                bool ok{ true };
-                for( auto& clause: clauseList ) if( ! clause.isSatisfied() ) // arbitrarily choose first unsatisfied assignment to randomly flip
+                randomInitialAssignment( clauseList );
+                for( auto i{ 0 }; i < 2 * N * N; ++i )
                 {
-                    ok = false;
-                    flipRandomAssignment( clause );
-                    break;
+                    bool ok{ true };
+                    for( auto& clause: clauseList ) if( ! clause.isSatisfied() ) // random literal assignment flip upon arbitrarily first found unsatisfied clause
+                    {
+                        ok = false;
+                        auto[ value, assignment ] = flipRandomAssignment( clause );
+                        for( auto& clause: clauseList ) // propagate random literal assignment flip from above for all clauses
+                        {
+                            if( value == clause.first.value ) clause.first.assignment = assignment;
+                            if( value == clause.second.value ) clause.second.assignment = assignment;
+                        }
+                        break;
+                    }
+                    if( ok ) return true;
                 }
-                if( ok ) return ok;
+            }
+            return false;
+        }
+
+    private:
+
+        using RandomDevice = random_device;
+        using Generator = mt19937;
+        using Distribution = uniform_int_distribution< int >;
+
+        bool randomAssignment( RandomDevice randomDevice=RandomDevice() )
+        {
+            Generator randomGenerator{ randomDevice() };
+            Distribution distribution{ 0, 1 };
+            return distribution( randomGenerator );
+        }
+
+        void randomInitialAssignment( ClauseList& clauseList )
+        {
+            for( auto& clause: clauseList )
+                clause.first.assignment = randomAssignment(),
+                clause.second.assignment = randomAssignment();
+        }
+
+        pair< Type, bool > flipRandomAssignment( Clause& clause, RandomDevice randomDevice=RandomDevice() )
+        {
+            Generator randomGenerator{ randomDevice() };
+            Distribution distribution{ 0, 1 };
+            auto first = distribution( randomGenerator ); // randomly flip the value of the first xor second literal assignment
+            if( first )
+            {
+                clause.first.assignment ^= 1;
+                return{ clause.first.value, clause.first.assignment };
+            }
+            else
+            {
+                clause.second.assignment ^= 1;
+                return{ clause.second.value, clause.second.assignment };
             }
         }
-        return false;
-    }
 
-private:
+    }; // class Solution
 
-    using RandomDevice = random_device;
-    using Generator = mt19937;
-    using Distribution = uniform_int_distribution< int >;
-
-    bool randomAssignment( RandomDevice randomDevice=RandomDevice() )
-    {
-        Generator randomGenerator{ randomDevice() };
-        Distribution distribution{ 0, 1 };
-        return distribution( randomGenerator );
-    }
-
-    void randomInitialAssignment( ClauseList& clauseList )
-    {
-        for( auto& clause: clauseList )
-            clause.first.assignment = randomAssignment(),
-            clause.second.assignment = randomAssignment();
-    }
-
-    void flipRandomAssignment( Clause& clause, RandomDevice randomDevice=RandomDevice() )
-    {
-        Generator randomGenerator{ randomDevice() };
-        Distribution distribution{ 0, 1 };
-        auto first = distribution( randomGenerator ); // randomly flip the value of the first xor second literal assignment
-        if( first )
-            clause.first.assignment ^= 1;
-        else
-            clause.second.assignment ^= 1;
-    }
-
-}; // class Solution
+} // namespace Papadimitriou
 
 
 ClauseList readInput( const string& filename, ClauseList clauseList={} )
@@ -102,7 +116,7 @@ ClauseList readInput( const string& filename, ClauseList clauseList={} )
     for( string line; getline( stream, line ); )
     {
         istringstream parser{ line };
-        if( N > 0 )
+        if( N > 0 ) // insert value/contraints into clauseList: negative values are changed to positive values ( "NOT" is expressed via false constraint )
         {
             Clause clause{ {0,true}, {0,true} };
             parser >> clause.first.value >> clause.second.value;
@@ -113,9 +127,11 @@ ClauseList readInput( const string& filename, ClauseList clauseList={} )
                 clause.second.value *= -1,
                 clause.second.constraint = false;
             clauseList.push_back( clause );
-            continue;
         }
-        parser >> N;
+        else
+        {
+            parser >> N;
+        }
     }
     return clauseList;
 }
@@ -123,16 +139,17 @@ ClauseList readInput( const string& filename, ClauseList clauseList={} )
 
 int main()
 {
-    Solution solution;
+    Papadimitriou::Solution solution;
     string ans;
     for( auto& filename: { "2sat1.txt", "2sat2.txt", "2sat3.txt", "2sat4.txt", "2sat5.txt", "2sat6.txt" } )
     {
         auto clauseList = readInput( filename );
         auto ok = solution.isSatisfiable( clauseList );
         ans.push_back( ok? '1': '0' );
+        cout << ( ok? '1': '0' ) << endl;
+        flush( cout );
     }
-    cout << "answer: " << ans << endl; // answer: 111111
-
+    cout << "answer: " << ans << endl; // answer: 101100
 
     return 0;
 }
